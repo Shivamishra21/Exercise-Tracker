e8a5431fa217ceb22549e1468ddab9344c527892
Shiva <shishy212002@gmail.com>
2021-06-12 13:16:44 +0530

    authentication

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,20 +1,55 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const {isEmail} = require('validator')
 
 //Schema building
 
 const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
+    email: {
+      type: String,
+      required: [true, "Please enter email!"],
+      unique: true,
+      lowercase: true,
+      validate:[isEmail,"Please enter a valid email."]
+    },
     username: {
       type: String,
-      required: true,
+      required: [true, "Please enter username!"],
       unique: true,
       trim: true,
       minlength: 4,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter password."],
+      minlength: [7, "Length of password should be 7."],
     },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model('User',userSchema);
-module.exports = User
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.statics.login = async function (username, password) {
+  const user = await this.findOne({
+    // $or: [{ username: username }, { email: username }],
+    username: username,
+  });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error("Incorrect Password");
+  }
+  throw Error("Incorrect Email/Username");
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,32 +1,92 @@
 const route = require("express").Router();
-const express = require('express')
-const app = express()
-app.use(express.json())
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const app = express();
+app.use(express.json());
 
 const User = require("../model/user");
+const { Error } = require("mongoose");
 
-route.get ('/',(req, res) => {
+const handleErrors = (err) => {
+    console.log(err)
+  let error = { username: "", password: "",email:"" };
+  if(err.message === 'Incorrect Email/Username'){
+    error.email = 'This email/username is not registered.'
+  } 
+
+  if(err.message === 'Incorrect Password'){
+    error.password = 'Password incorrect.'
+  }
+  if (err.code === 11000) {
+      if(err.keyValue.email){
+        error.email = " This email is already registered."; 
+      }
+      else if(err.keyValue.username)
+        error.username = " This username is already registered.";
+    return error;
+  }
+  if (err.message.includes("User validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      error[properties.path] = properties.message;
+    });
+  }
+  return error;
+};
+
+const maxAge = 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
+};
+
+route.get("/", (req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => {
-      res.status(400).json("Error: " + err)
+      res.status(400).json("Error: " + err);
       console.log(err);
     });
 });
 
-route.post('/add',(req,res)=>{
-    const username = req.body.username;
-    const data = {username:username}
-    User.create(data,(err,doc)=>{
-        if(err){
-            console.log("Error in adding User:-  ",err)
-        }
-        else{
-            console.log(doc)
-            res.send("User added!")
-        }
+route.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
+  const data = { username, password,email};
+  User.create(data)
+    .then((user) => {
+      const token = createToken(user._id);
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        maxAge: maxAge * 1000,
+      });
+      res.status(201).json({ user: { id: user._id, username: user.username } });
     })
+    .catch((err) => {
+      console.log("Error in user adding :-", err.message);
+      const errors = handleErrors(err);
+      res.status(400).json({ errors });
+    });
+});
 
+route.post('/login',(req,res)=>{
+    const {username,password} = req.body
+    User.login(username,password)
+    .then((user)=>{
+        const token =  createToken(user._id);
+        res.cookie('jwt',token,{
+            httpOnly:true,
+            maxAge:maxAge*1000
+        })
+        res.status(200).json({user:user.username})
+
+    })
+.catch((err)=>{
+    const errors = handleErrors(err)
+    res.status(400).json({errors})
+})
 })
 
-module.exports = route
+
+module.exports = route;
